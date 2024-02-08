91bbc047c02845e10f8e39acce51e21d193a7f34
nyck <ghost2394228@gmail.com>
2024-02-08 11:54:24 +0300

    2 step

---
+++ gendiff.ts
@@ -0,0 +1,13 @@
+import { Command } from 'commander';
+const program = new Command();
+
+program
+.name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+
+program.parse(process.argv);
+
+const options = program.opts();
