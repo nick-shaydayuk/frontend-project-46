cd99ec72d7e647392033e718eb393b6b4972cab1
nick <ghost2394228@gmail.com>
2024-02-19 15:54:26 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { PathLike, readFileSync } from 'node:fs';
+import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import parser from './parser';
 import getDiff from './getDiff';
-import formatter, { FormatCaseEnum } from './formatter';
+import formatter from './formatter';
 
 const resolvePath = (filePath) => path.resolve(process.cwd(), filePath);
 
