76a2b7d7a5988ae66d27403904ea4e77de0f441c
nick <ghost2394228@gmail.com>
2024-02-19 16:20:32 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ import gendiff from '../index.js';
 const program = new Command();
 
 program
+  .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
